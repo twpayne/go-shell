04839c71f6f4082027dba998938ddf3958b8ac8a
Tom Payne <twpayne@gmail.com>
2019-10-03 21:09:26 +0200

    Remove UserShell example

--- example_test.go
+++ example_test.go
@@ -2,24 +2,12 @@ package shell_test
 
 import (
 	"fmt"
-	"os/user"
 
 	"github.com/twpayne/go-shell"
 )
 
 func Example_shell_CurrentUserShell() {
 	shell, ok := shell.CurrentUserShell()
-	_ = ok
-	fmt.Println(shell)
-}
-
-func Example_shell_UserShell() {
-	u, err := user.Current()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	shell, ok := shell.UserShell(u)
 	_ = ok
 	fmt.Println(shell)
 }
