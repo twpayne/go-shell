e35f0c0c1eb0f3a893d30d71a4a1865988f9aeed
Tom Payne <twpayne@gmail.com>
2020-05-07 03:00:04 +0100

    Prioritize $SHELL environment variable

--- shell_posix.go
+++ shell_posix.go
@@ -14,39 +14,44 @@ const defaultShell = "/bin/sh"
 
 // CurrentUserShell returns the current user's shell.
 func CurrentUserShell() (string, bool) {
-	if u, err := user.Current(); err == nil {
-		// If getpwnam_r is available, use it.
-		if shell, ok := cgoGetUserShell(u.Username); ok {
-			return shell, true
-		}
-
-		// If getent is available, use it.
-		if getent, err := exec.LookPath("getent"); err == nil {
-			if output, err := exec.Command(getent, "passwd", u.Username).Output(); err == nil {
-				if fields := strings.SplitN(strings.TrimSuffix(string(output), "\n"), ":", 7); len(fields) == 7 {
-					return fields[6], true
-				}
-			}
-		}
-
-		// If the user has an entry in /etc/passwd, use it.
-		if f, err := os.Open("/etc/passwd"); err == nil {
-			defer f.Close()
-
-			s := bufio.NewScanner(f)
-			for s.Scan() {
-				if fields := strings.SplitN(strings.TrimSuffix(s.Text(), "\n"), ":", 7); len(fields) == 7 && fields[0] == u.Username {
-					return fields[6], true
-				}
-			}
-
-			_ = s.Err() // Ignore errors.
-		}
-	}
-
 	// If the SHELL environment variable is set, use it.
 	if shell, ok := os.LookupEnv("SHELL"); ok {
 		return shell, true
+	}
+
+	// Try to get the current user. If we can't then fallback to the default
+	// shell.
+	u, err := user.Current()
+	if err != nil {
+		return defaultShell, false
+	}
+
+	// If getpwnam_r is available, use it.
+	if shell, ok := cgoGetUserShell(u.Username); ok {
+		return shell, true
+	}
+
+	// If getent is available, use it.
+	if getent, err := exec.LookPath("getent"); err == nil {
+		if output, err := exec.Command(getent, "passwd", u.Username).Output(); err == nil {
+			if fields := strings.SplitN(strings.TrimSuffix(string(output), "\n"), ":", 7); len(fields) == 7 {
+				return fields[6], true
+			}
+		}
+	}
+
+	// If the user has an entry in /etc/passwd, use it.
+	if f, err := os.Open("/etc/passwd"); err == nil {
+		defer f.Close()
+
+		s := bufio.NewScanner(f)
+		for s.Scan() {
+			if fields := strings.SplitN(strings.TrimSuffix(s.Text(), "\n"), ":", 7); len(fields) == 7 && fields[0] == u.Username {
+				return fields[6], true
+			}
+		}
+
+		_ = s.Err() // Ignore errors.
 	}
 
 	// Fallback to the default shell.
